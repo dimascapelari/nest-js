8c5a50207915b33880e82e29038071a9dc6dc29b
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2026-05-26 09:05:57 -0300

    Interceptors - Aprendendo sobre

---
+++ src/common/interceptors/index.ts
@@ -0,0 +1,3 @@
+export * from './logger.interceptor';
+export * from './body-create-task.interceptor';
+export * from './add-header.interceptor';
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -8,17 +8,25 @@ import {
   Patch,
   Post,
   Query,
+  UseInterceptors,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import {
+  AddHeaderInterceptor,
+  BodyCreateTaskInteceptor,
+  LoggerInterceptor,
+} from '../common/interceptors';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly taskService: TasksService) {}
 
   @Get()
+  @UseInterceptors(LoggerInterceptor)
+  @UseInterceptors(AddHeaderInterceptor)
   findAllTasks(@Query() paginationDto: PaginationDto) {
     return this.taskService.findAll(paginationDto);
   }
@@ -34,6 +42,7 @@ export class TasksController {
   }
 
   @Post()
+  @UseInterceptors(BodyCreateTaskInteceptor)
   createTask(@Body() createTaskDto: CreateTaskDto) {
     return this.taskService.create(createTaskDto);
   }
