07c8dc8135cd3f5a6f4a7ad6e914ac9ce34b3c9a
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2026-05-27 09:18:17 -0300

    Estudando novos módulos e sobre a migrate

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [UsersController],
+  providers: [UsersService]
 })
 export class UsersModule {}
