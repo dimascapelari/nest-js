370c17bf2373c166ad09ffc0cd1a30765c956e3f
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2026-06-11 07:51:49 -0300

    conhecendo e configurando testes com jest

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,7 @@
+describe('UsersService', () => {
+  it('deveria testar o modulo usersservice', () => {});
+
+  it('should be define service', () => {});
+
+  test('testar se o users service foi definido', () => {});
+});
