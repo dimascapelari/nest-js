7dcabb2c439f8aec1e46c0478eb3b782253122ac
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2026-06-11 08:10:54 -0300

    primeiro teste automatizado

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,7 +1,26 @@
+// Testes unitários
+// Testes ponta a ponta (e2e)
+
+/*
+    => Padrão AAA
+
+     > Configuração do teste (Arrange)
+     > Algo que deseja fazer a ação (Act)
+     > Conferir se a ação foi esperada (Assert)
+*/
+
 describe('UsersService', () => {
-  it('deveria testar o modulo usersservice', () => {});
+  //   it('deveria testar o modulo usersservice', () => {});
 
-  it('should be define service', () => {});
+  //   test('testar se o users service foi definido', () => {});
 
-  test('testar se o users service foi definido', () => {});
+  it('should be define users service', () => {
+    const numero1 = 150;
+    const numero2 = 100;
+
+    const conta = numero1 - numero2;
+
+    // expect(conta).toBe(50);
+    expect(conta).toBeGreaterThan(10);
+  });
 });
