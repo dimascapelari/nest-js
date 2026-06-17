dc1b88fc337e196ad088d9af07caa7b441f8508c
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2026-06-17 11:02:28 -0300

    Teste falha no envio de arquivo

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -55,6 +55,7 @@ describe('UsersService', () => {
                 name: 'Testeteste',
                 email: 'testeteste@teste.com',
               }),
+              findMany: jest.fn(),
               findFirst: jest.fn(),
               update: jest.fn(),
               delete: jest.fn(),
@@ -86,7 +87,7 @@ describe('UsersService', () => {
       // Verificar se prisma user create foi chamado
       // O retorno deve ser o novo user criado
 
-      //  > Configuração do teste (Arrange)
+      // -> (Arrange)
       const createUserDto: CreateUserDto = {
         name: 'Testeteste',
         email: 'testeteste@teste.com',
@@ -95,10 +96,10 @@ describe('UsersService', () => {
 
       jest.spyOn(hashingService, 'hash').mockResolvedValue('HASH_MOCK_EXEMPLO');
 
-      // > Algo que deseja fazer a ação (Act)
+      // -> (Act)
       const result = await userService.create(createUserDto);
 
-      // > Conferir se a ação foi esperada (Assert)
+      // -> (Assert)
       expect(hashingService.hash).toHaveBeenCalled();
       expect(prismaService.user.create).toHaveBeenCalledWith({
         data: {
@@ -121,6 +122,7 @@ describe('UsersService', () => {
     });
 
     it('should throw error if prisma create fails', async () => {
+      // -> (Arrange)
       const createUserDto: CreateUserDto = {
         name: 'Testeteste',
         email: 'testeteste@teste.com',
@@ -132,6 +134,7 @@ describe('UsersService', () => {
         .spyOn(prismaService.user, 'create')
         .mockRejectedValue(new Error('Database error'));
 
+      // -> (Act / Assert)
       await expect(userService.create(createUserDto)).rejects.toThrow(
         new HttpException(
           'Falha ao cadastrar usuário!',
@@ -156,6 +159,32 @@ describe('UsersService', () => {
     });
   });
 
+  describe('FindAll Users', () => {
+    it('should return All Users', async () => {
+      // -> (Arrange)
+      const mockUsers = [
+        {
+          id: 1,
+          name: 'Dimas',
+          email: 'dimas@teste.com',
+          avatar: null,
+          passwordHash: 'hash_exemplo',
+          active: true,
+          createdAt: new Date(),
+        },
+      ];
+
+      jest.spyOn(prismaService.user, 'findMany').mockResolvedValue(mockUsers);
+
+      // -> (Act)
+      const result = await userService.findAll();
+
+      // -> (Assert)
+      expect(prismaService.user.findMany).toHaveBeenCalled();
+      expect(result).toEqual(mockUsers);
+    });
+  });
+
   describe('FindOnde User', () => {
     it('should return a findOne user', async () => {
       // -> (Arrange)
@@ -172,8 +201,10 @@ describe('UsersService', () => {
 
       jest.spyOn(prismaService.user, 'findFirst').mockResolvedValue(mockUser);
 
+      // -> (Act)
       const result = await userService.findOne(1);
 
+      // -> (Assert)
       expect(prismaService.user.findFirst).toHaveBeenCalledWith({
         where: {
           id: 1,
@@ -191,8 +222,10 @@ describe('UsersService', () => {
     });
 
     it('should throw error exception when user is not found', async () => {
+      // -> (Arrange)
       jest.spyOn(prismaService.user, 'findFirst').mockResolvedValue(null);
 
+      // -> (Act / Assert)
       await expect(userService.findOne(1)).rejects.toThrow(
         new HttpException('Usuário não encontrado!', HttpStatus.BAD_REQUEST),
       );
@@ -215,6 +248,7 @@ describe('UsersService', () => {
 
   describe('Update User', () => {
     it('should throw exception when user is not found', async () => {
+      // -> (Arrange)
       const updateUserDto: UpdateUserDto = { name: 'Novo nome' };
       const tokenPayload: PayloadTokenDto = {
         sub: 1,
@@ -225,8 +259,9 @@ describe('UsersService', () => {
         iss: '',
       };
 
-      jest.spyOn(prismaService.user, 'findFirst').mockRejectedValue(null);
+      jest.spyOn(prismaService.user, 'findFirst').mockResolvedValue(null);
 
+      // -> (Act / Assert)
       await expect(
         userService.update(1, updateUserDto, tokenPayload),
       ).rejects.toThrow(
@@ -238,6 +273,7 @@ describe('UsersService', () => {
     });
 
     it('should throw UNAUTHORIZED exception when user is not authorized', async () => {
+      // -> (Arrange)
       const updateUserDto: UpdateUserDto = { name: 'Novo nome' };
       const tokenPayload: PayloadTokenDto = {
         sub: 5,
@@ -259,8 +295,9 @@ describe('UsersService', () => {
         createdAt: new Date(),
       };
 
-      jest.spyOn(prismaService.user, 'findFirst').mockRejectedValue(mockUser);
+      jest.spyOn(prismaService.user, 'findFirst').mockResolvedValue(mockUser);
 
+      // -> (Act / Assert)
       await expect(
         userService.update(1, updateUserDto, tokenPayload),
       ).rejects.toThrow(
@@ -272,6 +309,7 @@ describe('UsersService', () => {
     });
 
     it('should user updated', async () => {
+      // -> (Arrange)
       const updateUserDto: UpdateUserDto = {
         name: 'Novo nome',
         password: 'nova senha',
@@ -309,8 +347,10 @@ describe('UsersService', () => {
       jest.spyOn(hashingService, 'hash').mockResolvedValue('novo_hash_exemplo');
       jest.spyOn(prismaService.user, 'update').mockResolvedValue(updatedUser);
 
+      // -> (Act)
       const result = await userService.update(1, updateUserDto, tokenPayload);
 
+      // -> (Assert)
       expect(hashingService.hash).toHaveBeenCalledWith(updateUserDto.password);
 
       expect(prismaService.user.update).toHaveBeenCalledWith({
@@ -334,6 +374,7 @@ describe('UsersService', () => {
 
   describe('Delete user', () => {
     it('should throw error when user is not found', async () => {
+      // -> (Arrange)
       const tokenPayload: PayloadTokenDto = {
         sub: 1,
         aud: '',
@@ -345,12 +386,14 @@ describe('UsersService', () => {
 
       jest.spyOn(prismaService.user, 'findFirst').mockResolvedValue(null);
 
+      // -> (Act / Assert)
       await expect(userService.delete(1, tokenPayload)).rejects.toThrow(
         new HttpException('Falha ao deletar usuário!', HttpStatus.BAD_REQUEST),
       );
     });
 
     it('should throw UNAUTHORIZED when user is not authorized', async () => {
+      // -> (Arrange)
       const tokenPayload: PayloadTokenDto = {
         sub: 5,
         aud: '',
@@ -372,6 +415,7 @@ describe('UsersService', () => {
 
       jest.spyOn(prismaService.user, 'findFirst').mockResolvedValue(mockUser);
 
+      // -> (Act / Assert)
       await expect(
         userService.delete(mockUser.id, tokenPayload),
       ).rejects.toThrow(
@@ -382,6 +426,7 @@ describe('UsersService', () => {
     });
 
     it('should delete user', async () => {
+      // -> (Arrange)
       const tokenPayload: PayloadTokenDto = {
         sub: 1,
         aud: '',
@@ -404,8 +449,10 @@ describe('UsersService', () => {
       jest.spyOn(prismaService.user, 'findFirst').mockResolvedValue(mockUser);
       jest.spyOn(prismaService.user, 'delete').mockResolvedValue(mockUser);
 
+      // -> (Act)
       const result = await userService.delete(mockUser.id, tokenPayload);
 
+      // -> (Assert)
       expect(prismaService.user.delete).toHaveBeenCalledWith({
         where: {
           id: mockUser.id,
@@ -420,6 +467,7 @@ describe('UsersService', () => {
 
   describe('Upload Avatar User', () => {
     it('should throw NOT_FOUND when user is not found', async () => {
+      // -> (Arrange)
       const tokenPayload: PayloadTokenDto = {
         sub: 1,
         aud: '',
@@ -437,6 +485,7 @@ describe('UsersService', () => {
 
       jest.spyOn(prismaService.user, 'findFirst').mockResolvedValue(null);
 
+      // -> (Act / Assert)
       await expect(
         userService.uploadAvatarImage(tokenPayload, file),
       ).rejects.toThrow(
@@ -448,6 +497,7 @@ describe('UsersService', () => {
     });
 
     it('should upload avatar and update user sucessfully', async () => {
+      // -> (Arrange)
       const tokenPayload: PayloadTokenDto = {
         sub: 1,
         aud: '',
@@ -481,10 +531,12 @@ describe('UsersService', () => {
       jest.spyOn(prismaService.user, 'update').mockResolvedValue(updateUser);
       jest.spyOn(fs, 'writeFile').mockResolvedValue();
 
+      // -> (Act)
       const result = await userService.uploadAvatarImage(tokenPayload, file);
 
       const fileLocale = path.resolve(process.cwd(), 'files', '1.png');
 
+      // -> (Assert)
       expect(fs.writeFile).toHaveBeenCalledWith(fileLocale, file.buffer);
       expect(prismaService.user.update).toHaveBeenCalledWith({
         where: {
@@ -502,6 +554,46 @@ describe('UsersService', () => {
       });
 
       expect(result).toEqual(updateUser);
+    });
+
+    it('should throw error if file fails', async () => {
+      // -> (Arrange)
+      const tokenPayload: PayloadTokenDto = {
+        sub: 1,
+        aud: '',
+        email: 'dimas@teste.com',
+        exp: 123,
+        iat: 123,
+        iss: '',
+      };
+
+      const file = {
+        originalname: 'avatar.png',
+        mimetype: 'image/png',
+        buffer: Buffer.from(''),
+      } as Express.Multer.File;
+
+      const mockUser: any = {
+        id: 1,
+        name: 'Dimas',
+        email: 'dimas@teste.com',
+        avatar: null,
+      };
+
+      jest.spyOn(prismaService.user, 'findFirst').mockResolvedValue(mockUser);
+      jest
+        .spyOn(fs, 'writeFile')
+        .mockRejectedValue(new Error('File write error'));
+
+      // -> (Act / Assert)
+      await expect(
+        userService.uploadAvatarImage(tokenPayload, file),
+      ).rejects.toThrow(
+        new HttpException(
+          'Falha ao atualizar o avatar do usuário!',
+          HttpStatus.BAD_REQUEST,
+        ),
+      );
     });
   });
 });
